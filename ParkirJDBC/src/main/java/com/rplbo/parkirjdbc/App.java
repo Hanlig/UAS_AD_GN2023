cf51cdaa943ee08f948d8781dc44358a5ae908e3
Antonius Rachmat <antoniusrc@users.noreply.github.com>
2023-06-08 13:34:32 +0700

    Add files via upload

---
+++ ParkirJDBC/src/main/java/com/rplbo/parkirjdbc/App.java
@@ -0,0 +1,14 @@
+package com.rplbo.parkirjdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ParkirApp pa = new ParkirApp();
+        
+    }
+}
---
+++ ParkirJDBC/src/main/java/com/rplbo/parkirjdbc/ParkirApp.java
@@ -0,0 +1,23 @@
+package com.rplbo.parkirjdbc;
+
+
+public class ParkirApp {
+    
+    public void koneksi(){
+        
+    }
+
+    public void tambahData(){
+        
+    }
+
+    public void tampilData(){
+        
+
+    }
+
+    public void totalPemasukan(){
+        
+
+    }
+}
